11df75aa64c0ac20b52a3f74a8cae3c82c2c0a72
lnmdsf <1478713159@qq.com>
2018-12-07 09:25:07 +0800

    第一次组织建的项目

---
+++ src/com/github/entity/test.java
@@ -0,0 +1,12 @@
+package com.github.entity;
+
+public class test {
+	public static void main(String[] args) {
+		test t = new test();
+		t.test1();
+	}
+	
+	public void test1() {
+		System.out.println("test1");
+	}
+}
